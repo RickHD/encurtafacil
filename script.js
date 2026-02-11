4edad7193a556760485779be19f68d67e6e092e0
oRichter <69596277+RickHD@users.noreply.github.com>
2026-02-11 16:00:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function encurtar() {
 
   // Simulação (troque depois pela API real)
   const slug = Math.random().toString(36).substring(2, 8);
-  const shortUrl = `${window.location.origin}/${slug}`;
+  const shortUrl = `https://encurtafacil.encurtafacil.workers.dev/${slug}`;
 
   const resultado = document.getElementById("resultado");
 
